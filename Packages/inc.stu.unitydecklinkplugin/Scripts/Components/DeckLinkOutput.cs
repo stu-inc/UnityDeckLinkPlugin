abdb5b1c8fbeba065369f3062566e49368d4a7ad
Naoto Kondo <cgigcp3yqt@gmail.com>
2021-07-26 21:08:45 +0900

    Get target texture data

--- Packages/inc.stu.unitydecklinkplugin/Scripts/Components/DeckLinkOutput.cs
+++ Packages/inc.stu.unitydecklinkplugin/Scripts/Components/DeckLinkOutput.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace DeckLinkPlugin
 {
     public class DeckLinkOutput : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField] RenderTexture _targetTexture;
 
+        void OnEnable()
+        {
+            // Start upload loop
+            StartCoroutine("UploadTexture");
         }
 
-        // Update is called once per frame
-        void Update()
+        private IEnumerator UploadTexture()
         {
+            while (true)
+            {
+                // Wait for end of frame
+                yield return new WaitForEndOfFrame();
 
+                if (_targetTexture == null)
+                    continue;
+
+                // Get target texture data
+                AsyncGPUReadback.Request(_targetTexture, 0, (request) =>
+                {
+                    var data = request.GetData<byte>();
+                    Debug.Log(data.Length);
+                });
+            }
+
+            yield return null;
         }
     }
 }
