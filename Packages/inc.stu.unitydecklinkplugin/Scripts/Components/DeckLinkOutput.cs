67a3c716bce8fc2d519c08d4c5d8570d5449b435
Naoto Kondo <cgigcp3yqt@gmail.com>
2021-07-27 16:32:34 +0900

    Add video frame to output stream

--- Packages/inc.stu.unitydecklinkplugin/Scripts/Components/DeckLinkOutput.cs
+++ Packages/inc.stu.unitydecklinkplugin/Scripts/Components/DeckLinkOutput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,37 @@ namespace DeckLinkPlugin
 {
     public class DeckLinkOutput : MonoBehaviour
     {
+        enum PixelFormat
+        {
+            YUV422_8bit = 0x32767579,
+            ARGB32_8bit = 32
+        }
+
+        [SerializeField] private int _deviceIndex = 0;
         [SerializeField] RenderTexture _targetTexture;
+        private IntPtr _outputStream;
 
         void OnEnable()
         {
+            IntPtr devices;
+            Debug.Log(DeckLinkCApi.ListDevices(out devices));
+            IntPtr device = DeckLinkCApi.GetDevice(devices, _deviceIndex);
+            _outputStream = DeckLinkCApi.CreateOutputStream(device);
+
+            DeckLinkCApi.StartOutputStream(_outputStream);
+
             // Start upload loop
             StartCoroutine("UploadTexture");
+        }
+
+        void OnDisable()
+        {
+            if (_outputStream != IntPtr.Zero)
+            {
+                DeckLinkCApi.StopOutputStream(_outputStream);
+                DeckLinkCApi.Release(_outputStream);
+                _outputStream = IntPtr.Zero;
+            }
         }
 
         private IEnumerator UploadTexture()
@@ -29,6 +55,7 @@ namespace DeckLinkPlugin
                 AsyncGPUReadback.Request(_targetTexture, 0, (request) =>
                 {
                     var data = request.GetData<byte>();
+                    DeckLinkPlugin.AddOutputStreamVideoFrame(_outputStream, data, 1920, 1080, 32);
                     Debug.Log(data.Length);
                 });
             }
